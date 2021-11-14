1679f206e2321749cb3fb993e8f43450053c74c5
ehmatthes <robertstipp17@gmail.com>
2021-11-14 05:02:15 -0800

    Added mongoose

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ app.use(express.static("public"));
 
 // TODO:
 
+// Database: Mongoose DB
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true});
+
+articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
+let articles = [];
+
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
